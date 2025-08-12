6a1aa290f6fd356a189cd7737fe3cffa79d10edb
Axyl S <axylschulz0@gmail.com>
2025-08-12 14:38:41 -0600

    Fix PlayerBot config resource path

--- build.bat
+++ build.bat
@@ -14,18 +14,24 @@ if errorlevel 1 (
 :: Step 3: Copy JSON config to build folder
 copy /Y PlayerBot.json build\ > nul
 
-:: Step 4: Detect bot API jar and create manifest file (2 lines!)
+:: Step 4: Detect bot API jar
 for %%F in (lib\robocode-tankroyale-bot-api-*.jar) do set "API_JAR=%%F"
 
+:: Step 5: Extract API jar into build folder
+pushd build
+jar xf ..\%API_JAR%
+rmdir /s /q META-INF 2>nul
+popd
+
+:: Step 6: Create manifest file
 > build\manifest.txt (
     echo Main-Class: Launcher
-    echo Class-Path: %API_JAR:\=/%
 )
 
-:: Step 5: Package JAR
+:: Step 7: Package JAR
 jar cfm PlayerBotLauncher.jar build\manifest.txt -C build .
 
-:: Step 6: Clean up
+:: Step 8: Clean up
 rmdir /s /q build
 
 endlocal
